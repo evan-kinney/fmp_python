b572838414179743859b011bc6f92c20741d4745
Evan Kinney <epkinney@gmail.com>
2022-02-10 14:41:01 -0700

    Create get_income_statement func

--- fmp_python/fmp.py
+++ fmp_python/fmp.py
@@ -146,5 +146,18 @@ class FMP(object):
         hp = self.__do_request__(rb.compile_request())
         return hp
 
+    @FMPDecorator.write_to_file
+    @FMPDecorator.format_data
+    def get_income_statement(self, symbol: str, period: str=None, limit: int=None):
+        rb = RequestBuilder(self.api_key)
+        rb.set_category('income-statement')
+        rb.add_sub_category(symbol)
+        if (period):
+            rb.add_query_param({'period': period})
+        if (limit):
+            rb.add_query_param({'limit': limit})
+        hp = self.__do_request__(rb.compile_request())
+        return hp
+
     def __do_request__(self,url):
         return requests.get(url)
